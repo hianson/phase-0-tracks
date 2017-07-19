86792fb0bad1bba0985d07e850c69ddf993f402b
Anson <leungans@gmail.com>
2017-07-19 14:47:33 -0700

    create hamster adoption form

---
+++ ruby/hamsters.rb
@@ -0,0 +1,18 @@
+puts "Hamster Adoption Form"
+puts "What is your hamster's name?"
+hamster_name = gets.chomp
+puts "What is the volume level of your hamster? (1-10)"
+hamster_volume = gets.chomp
+puts "What fur color does your hamster have?"
+hamster_color = gets.chomp
+puts "Would you recommend this hamster for adoption? (Y/N)"
+hamster_recommend = gets.chomp
+puts "Approximately what is the age of your hamster?"
+hamster_age = gets.chomp
+
+if hamster_age == ""
+  hamster_age = "n/a"
+  puts hamster_age
+else
+  puts hamster_age
+end
