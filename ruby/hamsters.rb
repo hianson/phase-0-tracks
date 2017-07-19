47a52e9f7de2c336a64aefb2f7a43f241dedd018
Anson <leungans@gmail.com>
2017-07-19 15:01:48 -0700

    convert user input to appropriate data types

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,16 +1,17 @@
 puts "Hamster Adoption Form"
 puts "What is your hamster's name?"
-hamster_name = gets.chomp
+hamster_name = gets.chomp.to_s
 puts "What is the volume level of your hamster? (1-10)"
-hamster_volume = gets.chomp
+hamster_volume = gets.chomp.to_i
 puts "What fur color does your hamster have?"
-hamster_color = gets.chomp
+hamster_color = gets.chomp.to_s
 puts "Would you recommend this hamster for adoption? (Y/N)"
-hamster_recommend = gets.chomp
+hamster_recommend = gets.chomp.to_s
+p hamster_recommend
 puts "Approximately what is the age of your hamster?"
-hamster_age = gets.chomp
+hamster_age = gets.chomp.to_i
 
-if hamster_age == ""
+if hamster_age == "".to_i
   hamster_age = "n/a"
   puts hamster_age
 else
