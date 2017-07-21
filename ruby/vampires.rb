0aaa51e5ded49cb57d988989b5854202e2313e42
Anson <leungans@gmail.com>
2017-07-21 15:56:11 -0700

    add conditional for name

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,16 +3,26 @@ name = gets.chomp
 puts "How old are you?"
 age = gets.chomp.to_i
 puts "What year were you born?"
-year = gets.chomp.to_i
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
+birth_year = gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
 garlic = gets.chomp
-puts "Would you like to enroll in the company’s health insurance? (Y/N)"
+puts "Would you like to enroll in the company’s health insurance? (yes/no)"
 insurance = gets.chomp
 
-if age == (2017 - year)
+if age == (2017 - birth_year)			#age is right
 	if garlic == "yes" || insurance == "yes"
-		puts "Probably not a vampire."
+		if name == "Drake Cula" || name == "Tu Fang"
+			puts "Definitely a vampire."
+		else
+			puts "Probably not a vampire."
+		end
+	end
+elsif age != (2017 - birth_year)			#age is wrong
+	if garlic == "no" && insurance == "no"
+		puts "Almost certainly a vampire."
+	elsif garlic == "no" || insurance == "no"
+		puts "Probably a vampire."
 	end
 else
-	puts "AGE WRONG!"
+	puts "Results inconclusive."
 end
