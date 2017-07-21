fbbb7e37c9f43da8266195cc05d51abc7830da67
Anson <leungans@gmail.com>
2017-07-21 16:13:47 -0700

    create global variable for employees to get number of surveys

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,45 +1,48 @@
-puts "How many employees will be processed?"
-employees = gets.chomp.to_i
+def vampireTest()
+	vampire_result = ""
+	i = 0
+	while i != $employees
+		puts "Employee Survey ##{i + 1}:"
+		puts "What is your name?"
+		name = gets.chomp
+		puts "How old are you?"
+		age = gets.chomp.to_i
+		puts "What year were you born?"
+		birth_year = gets.chomp.to_i
+		puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
+		garlic = gets.chomp
+		puts "Would you like to enroll in the company’s health insurance? (yes/no)"
+		insurance = gets.chomp
 
-vampire_result = ""
-i = 0
-while i != employees
-	puts "Employee Survey ##{i + 1}:"
-	puts "What is your name?"
-	name = gets.chomp
-	puts "How old are you?"
-	age = gets.chomp.to_i
-	puts "What year were you born?"
-	birth_year = gets.chomp.to_i
-	puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
-	garlic = gets.chomp
-	puts "Would you like to enroll in the company’s health insurance? (yes/no)"
-	insurance = gets.chomp
-
-	if age == (2017 - birth_year)			#age is right
-		if garlic == "yes" || insurance == "yes"
-			if name == "Drake Cula" || name == "Tu Fang"
-				vampire_result = "Definitely a vampire."
-			else
-				vampire_result = "Probably not a vampire."
+		if age == (2017 - birth_year)			#age is right
+			if garlic == "yes" || insurance == "yes"
+				if name == "Drake Cula" || name == "Tu Fang"
+					vampire_result = "Definitely a vampire."
+				else
+					vampire_result = "Probably not a vampire."
+				end
 			end
+		elsif age != (2017 - birth_year)			#age is wrong
+			if garlic == "no" && insurance == "no"
+				vampire_result = "Almost certainly a vampire."
+			elsif garlic == "no" || insurance == "no"
+				vampire_result = "Probably a vampire."
+			end
+		else
+			vampire_result = "Results inconclusive."
 		end
-	elsif age != (2017 - birth_year)			#age is wrong
-		if garlic == "no" && insurance == "no"
-			vampire_result = "Almost certainly a vampire."
-		elsif garlic == "no" || insurance == "no"
-			vampire_result = "Probably a vampire."
-		end
-	else
-		vampire_result = "Results inconclusive."
-	end
 
-	puts "Results:"
-	puts "Name: #{name}"
-	puts "Age: #{age}"
-	puts "Birth year: #{birth_year}"
-	puts "Likes garlic: #{garlic}"
-	puts "Health insurance: #{insurance}"
-	puts "#{vampire_result}"
-	i += 1
+		puts "Results:"
+		puts "Name: #{name}"
+		puts "Age: #{age}"
+		puts "Birth year: #{birth_year}"
+		puts "Likes garlic: #{garlic}"
+		puts "Health insurance: #{insurance}"
+		puts "#{vampire_result}"
+		i += 1
+	end
 end
+
+puts "How many employees will be processed?"
+$employees = gets.chomp.to_i
+vampireTest()
