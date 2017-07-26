17bf1d3eca863df7f291a6b5bedcda7b9ac91113
Anson <leungans@gmail.com>
2017-07-26 01:01:15 -0700

    add some pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,8 @@
+# Prompt designer/user for all information
+# Convert user input to appropriate data type
+# Print information back to user once user has entered all questions
+# Allow user to update keys. Otherwise if "none", skip update
+# If user types "decor_theme", ask user for new value for that key, then update it
+# Print latest version of hash and end program
+
+# Ask user for name, age, number of children, decor theme, color, pet preference
