437d1c0bc9f0add6bf9d44e50f42aebe97100dd8
Anson <leungans@gmail.com>
2017-07-26 18:08:10 -0700

    write a working loop with array

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -5,4 +5,22 @@
 # Otherwise:
 	# return nil
 
-	
+	arr = [69, 420, 1337, 80085]
+
+	def search_array(arr, int)
+	  #if int is found in arr
+	    #return index of int
+
+	    i = 0
+	    while i < arr.length
+	      # if int matches the position of loop
+	      if int == arr[i]
+	        puts "int exists"
+	      else
+	        puts "int doesn't exist"
+	      end
+	      i += 1
+	    end
+	end
+
+	search_array(arr, 420)
