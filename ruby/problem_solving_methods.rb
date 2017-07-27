362294a4942018ab4f78fd0c0c592abcc5bac95f
Anson <leungans@gmail.com>
2017-07-26 17:59:39 -0700

    add some pseudocode

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,8 @@
+# Release 0: Implement a Simple Search
+# search_array method takes in an array and integer as arguments
+# If integer exists in array
+	# return index of that integer
+# Otherwise:
+	# return nil
+
+	
