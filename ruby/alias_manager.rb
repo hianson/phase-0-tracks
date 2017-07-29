7c9d34171e12a083a9e3fdb0a768014aee9077c5
Anson <leungans@gmail.com>
2017-07-29 15:57:32 -0700

    improve edge cases

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -14,7 +14,11 @@ def alias_manager(first_last)
 	i = 0
 	while i < last_first.length
 		#if [i] is vowel...
-	  if last_first[i].count("^#{vowels}").zero?
+    if last_first[i] == "u"
+      last_first[i] = "a"
+    elsif last_first[i] == "z"
+      last_first[i] = "b"
+	  elsif last_first[i].count("^#{vowels}").zero?
 	    # we gonna change the vowel so get vowel index!
 	    vowel_index = vowels.index("#{last_first[i]}") + 1
 	    # change the old vowel in the string to the new one
@@ -23,15 +27,10 @@ def alias_manager(first_last)
     elsif last_first[i].count("^#{consonants}").zero?
       consonant_index = consonants.index("#{last_first[i]}") + 1
       last_first[i] = consonants[consonant_index]
-    elsif last_first[i] == vowels[-1]
-      last_first[i] = "a"
-    elsif last_first[i] == consonants[-1]
-      last_first[i] = "b"
 		end
 		i += 1
 	end
 	p last_first.split(" ").each {|word| word.capitalize!}.join(" ")
-
 end
 
 #returns true! :)
