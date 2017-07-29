6bb4244d4e37c05716d7c2e2e1c5e957fd430d39
Anson <leungans@gmail.com>
2017-07-29 03:00:11 -0700

    add some nested data structures

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,38 @@
+tiny_zoo = {
+	mammals: {
+		number: 3,
+		species: {
+			panda: {
+				number: 5,
+				food: [
+					'bamboo'
+				],
+				colors: [
+					'black',
+					'white'
+				]
+			},
+			giraffe: {
+				number: 4,
+				food: [
+					'fruits',
+					'vegetables'
+				],
+				colors: [
+					'yellow',
+					'brown'
+				]
+			},
+			oryx: {
+				number: 6,
+				food: [
+					'grass'
+				],
+				colors: [
+					'black',
+					'white'
+				]
+			}
+		}
+	}
+}
