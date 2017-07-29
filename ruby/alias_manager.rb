fc051cf5beb30a08b4180d48d60a1a5248976563
Anson <leungans@gmail.com>
2017-07-29 14:53:35 -0700

    init loop counter, handle some edge cases

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,7 +8,23 @@
 	# else if consonant: +1 position of consonant array
 
 	def alias_manager(first_last)
-		last_first = first_last.split(" ").reverse.join(" ")
+		last_first = first_last.split(" ").reverse.join(" ").downcase
+		vowels = "aeiou" #handle 'u' as edge case?
+		consonants = "bcdfghjklmnpqrstvwxyz" #handle 'z' as edge case?
+		i = 0
+		while i < last_first.length
+		  # if [i] is a u, go back to a
+		  if last_first[i] == "u"
+		    last_first[i] = "a"
+	    # if [i] is z, go back to b
+		  elsif last_first[i] == "z"
+		    last_first[i] = "b"
+	    # if [i] is other vowel
+		  end
+		i += 1
+		end
+		p last_first
 	end
 
-	p alias_manager("Sgt Pepperino")
+
+	alias_manager("Sgt Pupperz")
