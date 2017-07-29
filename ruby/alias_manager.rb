f6f49ea014ec14dd58c6411691a7d278244b9aea
Anson <leungans@gmail.com>
2017-07-29 14:19:52 -0700

    add some pseudocode for alias manager

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,8 @@
+# argument is first_last name
+# swap first/last name: make last_first name equal to .split.reverse of first_last
+# make an array of vowels [A, E, I, O, U]
+# make an array of consonants [B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Y, Z]
+# loop through each letter of last_first
+# for each i:
+	# if vowel: +1 position of vowel array
+	# else if consonant: +1 position of consonant array
