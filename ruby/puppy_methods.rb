37f980a43df67cf598de51d9524478371436d16a
Anson <leungans@gmail.com>
2017-08-01 14:19:08 -0700

    add some methods in our puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,31 @@ class Puppy
     toy
   end
 
+	def speak(int)
+		int.times do |i|
+			puts "Woof!"
+		end
+	end
+
+	def roll_over()
+		puts "*rolls over*"
+	end
+
+	def dog_years(int)
+		 return int * 7
+	end
+
+	def jump(int)
+		puts "*jumps*" * int
+	end
+
 end
 
+# Driver code:
+fido = Puppy.new
+
+fido.fetch("stick")
+fido.speak(3)
+fido.roll_over
+puts fido.dog_years(5)
+fido.jump(3)
