dfe9bbf1af0ca0dc0f66defe216177a7eaabfb3e
Anson <leungans@gmail.com>
2017-08-01 14:47:21 -0700

    create kitty class and methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -23,13 +23,48 @@ class Puppy
 		puts "*jumps*" * int
 	end
 
+	def initialize()
+		puts "Initializing new puppy instance ..."
+	end
+
+end
+
+class Cat
+	def initialize()
+		puts "I'm a new cat!"
+	end
+
+	def meow(int)
+		puts "Meow!" * int
+	end
+
+	def scratch(int)
+		puts "*scratch*" * int
+	end
+
 end
 
 # Driver code:
 fido = Puppy.new
 
-fido.fetch("stick")
-fido.speak(3)
-fido.roll_over
-puts fido.dog_years(5)
-fido.jump(3)
+# fido.fetch("stick")
+# fido.speak(3)
+# fido.roll_over
+# puts fido.dog_years(5)
+# fido.jump(3)
+
+kitty = []
+i = 1
+loop do
+	kitty.push(Cat.new)
+	puts "#{i}"
+	i += 1
+	break if i == 51
+end
+
+kitty.each do |cat|
+	cat.meow(3)
+	cat.scratch(2)
+end
+
+puts kitty
