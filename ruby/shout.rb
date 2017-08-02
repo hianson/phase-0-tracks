846332d58201c6600945bee89d2d37db9055dffa
Anson <leungans@gmail.com>
2017-08-02 13:39:18 -0700

    convert shout to a mixin and use with classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,32 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+# 	def self.yelling_happily(words)
+# 		words + "!!!!!!!!" + " :)"
+# 	end
+# end
+#
+# p Shout.yell_angrily("grrr")
+# p Shout.yelling_happily("wrrrr")
+
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
-	end
-	def self.yelling_happily(words)
-		words + "!!!!!!!!" + " :)"
+	def yell_angrily(words)
+		words + "!!!" + " >:("
 	end
 end
 
-p Shout.yell_angrily("grrr")
-p Shout.yelling_happily("wrrrr")
+class Mom
+	include Shout
+end
+
+class Wookie
+	include Shout
+end
+
+# Driver code
+wookie = Wookie.new
+p wookie.yell_angrily("RRRRRRRRrrrrrg")
+
+mom = Mom.new
+p mom.yell_angrily("take out da trash dis instinct anton")
