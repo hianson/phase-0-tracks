da55bb65c72aa1ebd8bce256db81960a7cbb1157
Anson <leungans@gmail.com>
2017-08-03 00:26:48 -0700

    pseudocode rspec for game

---
+++ ruby/game/game_spec.rb
@@ -0,0 +1,16 @@
+# 6.7 Solo Challenge
+
+# User can enter a word (or phrase) and another user attempts to guess that word
+# Guesses are limited, max guesses are related to length of word
+# Repeated guesses don't count (store previous guesses in array)
+# Guesser gets continual feedback on state of word:
+	# unicorn : _ _ _ c _ _ _
+# Give congrats for winning or taunting message for losing
+
+# our game should ...
+# allow user 1 to enter a word
+# allow user 2 to enter (guess) a word which matches user 1's word
+# see if user 2's guess matches user 1's word
+# have a variable which keeps track of user 2's guess count (repeated guesses don't count)
+# print the current state of user 2's guesses unicorn : _ _ _ c _ _ _
+# print a congrats message for winning, and taunt message for losing
