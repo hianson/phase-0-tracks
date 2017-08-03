bf65ca4909625a4c787ed16b888e0bdce30f5cde
Anson <leungans@gmail.com>
2017-08-03 00:20:11 -0700

    make game files and add pseudocode

---
+++ ruby/game/game.rb
@@ -0,0 +1,8 @@
+# 6.7 Solo Challenge
+
+# User can enter a word (or phrase) and another user attempts to guess that word
+# Guesses are limited, max guesses are related to length of word
+# Repeated guesses don't count (store previous guesses in array)
+# Guesser gets continual feedback on state of word:
+	# unicorn : _ _ _ c _ _ _
+# Give congrats for winning or taunting message for losing
