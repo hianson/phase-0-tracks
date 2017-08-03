46d2dce504b9bfd25703a47dae118f39e4e4b2b8
Anson <leungans@gmail.com>
2017-08-03 00:35:04 -0700

    translate pseudo-rspec and create game class

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -10,7 +10,38 @@
 # our game should ...
 # allow user 1 to enter a word
 # allow user 2 to enter (guess) a word which matches user 1's word
+# use an array to keep track of which letters user 2 has guessed
 # see if user 2's guess matches user 1's word
 # have a variable which keeps track of user 2's guess count (repeated guesses don't count)
 # print the current state of user 2's guesses unicorn : _ _ _ c _ _ _
 # print a congrats message for winning, and taunt message for losing
+
+# RSpec
+# 'describe' block for each group of tests
+  # 'it' block for each individual test
+    # expect(<YOUR CODE>).to eq <RESULT>
+
+require_relative 'game'
+
+describe Game do
+	it "allows user 1 to enter a word" do
+	end
+
+	it "allows user 2 to enter a word which matches user 1's word" do
+	end
+
+	it "uses an array to keep track of which letters user 2 has guessed" do
+	end
+
+	it "sees if user 2's guess matches user 1's word" do
+	end
+
+	it "has a variable which keeps track of user 2's guess count (repeated guesses don't count)" do
+	end
+
+	it "prints the current state of user 2's guesses" do
+	end
+
+	it "prints a congratulations or taunt message for winning or losing" do
+	end
+end
