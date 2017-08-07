850f5ea382d54dc89fc27422cda33ba69f3635bf
Anson <leungans@gmail.com>
2017-08-07 11:48:09 -0700

    complete release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,18 @@
 var colors = ["blue", "green", "indigo", "red"];
 var names = ["Bobby", "Daryl", "Luke", "Rocket"];
+
+colors.push("orange");
+names.push("Freddy");
+
+console.log(colors);
+console.log(names);
+
+// Create an object in JS:
+var horses = {};
+for (var i = 0; i < names.length; i++) {
+	for (var j = 0; j < colors.length; j++) {
+		horses[names[i]] = colors[i];
+	}
+
+}
+console.log(horses);
