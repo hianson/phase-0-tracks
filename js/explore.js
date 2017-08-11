c189ee7044d9633acc8cd1b56e773246b1d6b903
Anson <leungans@gmail.com>
2017-08-10 23:41:30 -0700

    take notes on release 0

---
+++ js/explore.js
@@ -0,0 +1,11 @@
+// Release 0:
+// What is the equivalent of nil in JavaScript?
+		//'null' in JS is the equivalent of 'nil' in Ruby
+// When you declare variables in JavaScript, it's a good idea to use the var keyword (you can Google it if you're interested, or just take our word for it). We can see other differences as well: how is the naming convention in JavaScript different from Ruby?
+		//Naming variables and methods seems to be in a camel-hump style in JS, as opposed to using underscores in Ruby.
+// How do you print a variable in JavaScript?
+		// Print a variable by simply typing out the varName outside of the quotes, and added to the rest of the string console.log("string" + varName);
+// How do you end a line of code in JavaScript?
+		// Use semicolons; to end lines in JS.
+// How do you write comments in JavaScript?
+		// Forward slashes -> //
