c73b346010ef98cc1bc0a865aeb332a8df7b5b37
Anson <leungans@gmail.com>
2017-08-11 18:49:07 -0700

    write some code for longestWord function

--- js/algos.js
+++ js/algos.js
@@ -3,16 +3,17 @@
 // Pseudocode function:
 function longestWord(word_arr) {
 // create variable to hold longest item in array argument
-var longest = ""
+var longest = "";
 	// iterate through array items
 	for (var i = 0; i < word_arr.length; i++) {
-	// get the length of each item
-		for (var j = 0; j < word_arr[i].length; j++) {
-			console.log(word_arr[i][j])
+		// get the length of each item
+		if (word_arr[i].length > longest.length) {
+			longest = word_arr[i];
 		}
 	}
+	return longest
 }
 // if the item's length is greatest, set it to the longest item variable
 
-word_arr = ["long phrase","longest phrase","longer phrase"]
-longestWord(word_arr);
+word_arr = ["long phrase","longest phrase","longer phrase"];
+console.log(longestWord(word_arr));
