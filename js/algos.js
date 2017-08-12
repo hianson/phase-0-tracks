1ea0e913e7befd465c3956a8dfa03a6cfe83ab75
Anson <leungans@gmail.com>
2017-08-11 18:42:06 -0700

    write base of nested loop for longestWord function

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,18 @@
 // Release 0
 // write a function that takes an array of words or phrases and returns the longest word or phrase in the array
-// Pseudocode:
+// Pseudocode function:
+function longestWord(word_arr) {
 // create variable to hold longest item in array argument
-// iterate through array items
-// get the length of each item
+var longest = ""
+	// iterate through array items
+	for (var i = 0; i < word_arr.length; i++) {
+	// get the length of each item
+		for (var j = 0; j < word_arr[i].length; j++) {
+			console.log(word_arr[i][j])
+		}
+	}
+}
 // if the item's length is greatest, set it to the longest item variable
+
+word_arr = ["long phrase","longest phrase","longer phrase"]
+longestWord(word_arr);
