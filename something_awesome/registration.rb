eca7bb858a9f236c745412cae2b95a2b91718521
Anson <leungans@gmail.com>
2017-08-16 12:52:14 -0700

    write some p-code for registration methods

--- something_awesome/registration.rb
+++ something_awesome/registration.rb
@@ -1,5 +1,36 @@
 # Allow user to create an account
 
 def register
+
+	# Ask for a password
+end
+
+
+def get_username
+	# Ask for a username
+
+	# Check if username already exists in database
+
+	# If username doesn't exist:
+		# Save username as a variable to return later
+		# Otherwise if username already exists:
+			# Notify user and make them try again
+
+	# Return a username
+end
+
+def get_password
+	# Ask for a password
+
+	# Create variable to hold password
+
+	# Ask user to repeat password
+		# If user repeats password successfully
+		# Return the password
+		# Otherwise:
+			# Ask user to try again
+end
+
+def check_database
 	
 end
