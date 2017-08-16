344eeb15b192988d318b24f7a3cb6f99527f3bb8
Anson <leungans@gmail.com>
2017-08-16 10:38:01 -0700

    write some awesome goals in comments to achieve for project

---
+++ something_awesome/awesome.rb
@@ -0,0 +1,7 @@
+# 'Create something awesome'
+# We don't know what to make, so we're just going to make random parts of something useful
+# Be able to:
+# Create user data
+# Retrieve user data
+# Manipulate user data
+# Provide access to information with user authentication
