d9f8085bc41d5395aa1a8b7855c9959716f0c596
Anson <leungans@gmail.com>
2017-08-17 18:56:47 -0700

    write some code for tracker method

--- something_awesome/tracker.rb
+++ something_awesome/tracker.rb
@@ -1,17 +1,19 @@
-def add_receipt
 # Method to track purchases
-
-# User should be able to add a receipt
-
-# Give user option to add a receipt
-
-# Ask user for item name
-
-# Ask user for quantity of item
-
-# Ask user for cost of item
-
-# Record date as current date
-
-# Insert the information into the purchases table of the database
+def add_receipt(database)
+	# Get the user's login_id from database to add receipt to appropriate username
+	
+	# Ask user for item name
+	puts "Enter name of item:"
+	item = gets.chomp
+	# Ask user for quantity of item
+	puts "Enter quantity of item:"
+	quantity = gets.chomp.to_i
+	# Ask user for cost of item
+	puts "Enter cost of item:"
+	cost = gets.chomp.to_f
+	# Record date as current date
+	date = Time.new.utc
+	# Insert the information into the purchases table of the database
+	database.execute("INSERT INTO purchases (item, quantity, cost, date, login_id) VALUES ('#{item}', '#{quantity}', '#{cost}', '#{date}', '#{login_id}');")
+	# Implicitly return the SQL command, add to database in the driver code
 end
