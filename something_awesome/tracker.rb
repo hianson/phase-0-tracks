bf468e64188f220c88b1f2122c6278bc3abb309d
Anson <leungans@gmail.com>
2017-08-17 18:38:16 -0700

    write some p-code for tracker method

--- something_awesome/tracker.rb
+++ something_awesome/tracker.rb
@@ -1,2 +1,17 @@
-# Track spending
+def add_receipt
+# Method to track purchases
+
 # User should be able to add a receipt
+
+# Give user option to add a receipt
+
+# Ask user for item name
+
+# Ask user for quantity of item
+
+# Ask user for cost of item
+
+# Record date as current date
+
+# Insert the information into the purchases table of the database
+end
