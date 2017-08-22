5001c361de8477aef52d7d1507e4763b0244e66c
Anson <leungans@gmail.com>
2017-08-22 16:59:53 -0700

    test some more dom manipulation

--- web_dev/client_side_js/index.js
+++ web_dev/client_side_js/index.js
@@ -7,14 +7,22 @@ function addBorder(event) {
 	event.target.style.border = "2px dotted purple";
 }
 
+function changeColor(event) {
+	var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+	var random_color = colors[Math.floor(Math.random()*colors.length)]
+	event.target.style.color = random_color;
+}
+
 function keyUp() {
 	var x = document.getElementById("name");
 	x.value = x.value.toUpperCase();
 }
-
 
 var paragraph1 = document.getElementById("paragraph_id");
 paragraph1.addEventListener("click", boldText);
 
 var paragraph2 = document.getElementById("paragraph_id2");
 paragraph2.addEventListener("click", addBorder);
+
+var paragraph3 = document.getElementById("paragraph_id3");
+paragraph3.addEventListener("click", changeColor);
