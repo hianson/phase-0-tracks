038197e852471374a73f1d57854d5dc980b5ea0f
Anson <leungans@gmail.com>
2017-08-23 16:23:20 -0700

    write some parameter and query routes in sinatra

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,47 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write a GET route that displays
+# an address
+
+get '/contact' do
+	address = "123 Main St."
+	"Contact us at #{address}"
+end
+
+# write a GET route that takes a person's name
+# as a query parameter and say "Good job, [person's name]!"
+# else if no parameter, say "Good job!"
+get '/great_job' do
+	if params[:name] == nil
+		"Good job!"
+	else
+		"Good job, #{params[:name]}!"
+	end
+end
+
+# route which adds two numbers and responds with result
+get '/:n1/:n2' do
+	n1 = params[:n1].to_s
+	n2 = params[:n2].to_s
+	"#{n1.to_i + n2.to_i}"
+end
+
+# route which lets user search db
+
+get '/campus' do
+	if params[:name] == nil
+		"Please enter a 'name' query parameter for a campus (SF, SD, SEA, CHI, NYC)."
+	else
+	  campus = db.execute("SELECT * FROM students where campus=?", params[:name])
+	  response = ""
+	  campus.each do |student|
+	    response << "ID: #{student['id']}<br>"
+	    response << "Name: #{student['name']}<br>"
+	    response << "Age: #{student['age']}<br>"
+	    response << "Campus: #{student['campus']}<br><br>"
+	  end
+	  response
+	end
+end
